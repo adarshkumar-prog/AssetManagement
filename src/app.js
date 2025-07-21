7889ce8f9311fff2e29106cd687e47be8c7a29d2
Adarsh <adarshkumarjha193@gmail.com>
2025-07-21 12:32:55 +0530

    Fixed cors

--- src/app.js
+++ src/app.js
@@ -7,9 +7,25 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger'); 
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'http://localhost:3000',
+  'https://assetmanagement.setside.app'
+];
+
 app.use(cors({
-     origin: '*',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 
 app.use(express.json());
