23e2818768d1c5b550a0ab1959b44fc8c5a631fc
Adarsh Kumar Jha <adarshkumarjha193@gmail.com>
2025-08-06 17:56:00 +0530

    currently removed calm api

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger'); 
 const cors = require('cors');
-const calmapi = require('calmapi');
 
 
 
